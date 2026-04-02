fafe1880c6f611373439d60e21af038f72812045
Ben Tomasik <tomislacker@users.noreply.github.com>
2026-04-02 13:07:39 -0400

    feat(infra): add Route53 A and AAAA records for hostname
    Create alias records pointing buckheadbutcher.noqa.io to the CloudFront distribution for both IPv4 and IPv6, enabling direct hostname access instead of CloudFront URL

--- infra/main.tf
+++ infra/main.tf
@@ -350,3 +350,29 @@ resource "aws_cloudfront_distribution" "site" {
 
   depends_on = [aws_acm_certificate_validation.site]
 }
+
+# Route53 A record pointing to CloudFront distribution
+resource "aws_route53_record" "site_a" {
+  zone_id = data.aws_route53_zone.main.zone_id
+  name    = var.hostname
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.site.domain_name
+    zone_id                = aws_cloudfront_distribution.site.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
+
+# Route53 AAAA record pointing to CloudFront distribution
+resource "aws_route53_record" "site_aaaa" {
+  zone_id = data.aws_route53_zone.main.zone_id
+  name    = var.hostname
+  type    = "AAAA"
+
+  alias {
+    name                   = aws_cloudfront_distribution.site.domain_name
+    zone_id                = aws_cloudfront_distribution.site.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
